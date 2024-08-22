59f4abd73c9592ab15acdff1286a89f4fb1cd501
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-08-22 22:40:27 +0000

    UTF-8 Validate

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,26 +6,25 @@ set represents a valid UTF-8 encoding.
 
 def validUTF8(data):
     """
-    Determines if a given data set represents a valid UTF-8 encoding.
+    Determine if a given data set represents a valid UTF-8 encoding.
 
     :param data: List of integers representing bytes of data
     :return: True if data is a valid UTF-8 encoding, else False
     """
-    # Number of bytes in the current UTF-8 character
     num_bytes = 0
 
-    # Masks for checking the most significant bits of a byte
-    mask1 = 1 << 7  # 10000000 in binary
-    mask2 = 1 << 6  # 01000000 in binary
+    # Masks to identify the significant bits of the byte
+    mask1 = 1 << 7  # 10000000
+    mask2 = 1 << 6  # 01000000
 
     for byte in data:
-        # Extract only the 8 least significant bits of the byte
+        # Get only the 8 least significant bits of the byte
         byte = byte & 0xFF
 
         if num_bytes == 0:
             # Determine the number of bytes in the UTF-8 character
             if byte & mask1 == 0:
-                # 1-byte character (ASCII)
+                # 1-byte character
                 continue
             elif byte & (mask1 >> 1) == mask1:
                 # 2-byte character
@@ -37,13 +36,13 @@ def validUTF8(data):
                 # 4-byte character
                 num_bytes = 3
             else:
-                # Invalid UTF-8 character
+                # Invalid first byte
                 return False
         else:
-            # Check that this byte is a valid continuation byte (10xxxxxx)
-            if byte & mask1 == mask1 and byte & mask2 == 0:
-                num_bytes -= 1
-            else:
+            # The byte must be a continuation byte (10xxxxxx)
+            if not (byte & mask1 and not (byte & mask2)):
                 return False
+            num_bytes -= 1
 
+    # If num_bytes is not zero, we have an incomplete character
     return num_bytes == 0
