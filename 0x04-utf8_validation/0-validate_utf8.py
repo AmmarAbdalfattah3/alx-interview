aa971c1dd918c78fb1d33d25ff04b765ecabec66
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-08-22 22:44:33 +0000

    UTF-8 Validate

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,6 +4,7 @@ set represents a valid UTF-8 encoding.
 """
 
 
+
 def validUTF8(data):
     """
     Determine if a given data set represents a valid UTF-8 encoding.
@@ -11,38 +12,38 @@ def validUTF8(data):
     :param data: List of integers representing bytes of data
     :return: True if data is a valid UTF-8 encoding, else False
     """
-    num_bytes = 0
+    num_bytes_to_process = 0
 
     # Masks to identify the significant bits of the byte
     mask1 = 1 << 7  # 10000000
     mask2 = 1 << 6  # 01000000
 
     for byte in data:
-        # Get only the 8 least significant bits of the byte
+        # Keep only the 8 least significant bits of the byte
         byte = byte & 0xFF
 
-        if num_bytes == 0:
+        if num_bytes_to_process == 0:
             # Determine the number of bytes in the UTF-8 character
-            if byte & mask1 == 0:
-                # 1-byte character
+            if (byte & mask1) == 0:
+                # 1-byte character (starts with 0xxxxxxx)
                 continue
-            elif byte & (mask1 >> 1) == mask1:
-                # 2-byte character
-                num_bytes = 1
-            elif byte & (mask1 >> 2) == mask1:
-                # 3-byte character
-                num_bytes = 2
-            elif byte & (mask1 >> 3) == mask1:
-                # 4-byte character
-                num_bytes = 3
+            elif (byte & (mask1 >> 1)) == mask1:
+                # 2-byte character (starts with 110xxxxx)
+                num_bytes_to_process = 1
+            elif (byte & (mask1 >> 2)) == mask1:
+                # 3-byte character (starts with 1110xxxx)
+                num_bytes_to_process = 2
+            elif (byte & (mask1 >> 3)) == mask1:
+                # 4-byte character (starts with 11110xxx)
+                num_bytes_to_process = 3
             else:
-                # Invalid first byte
+                # Invalid byte (does not match any valid UTF-8 header)
                 return False
         else:
-            # The byte must be a continuation byte (10xxxxxx)
+            # The byte must be a continuation byte (starts with 10xxxxxx)
             if not (byte & mask1 and not (byte & mask2)):
                 return False
-            num_bytes -= 1
+            num_bytes_to_process -= 1
 
-    # If num_bytes is not zero, we have an incomplete character
-    return num_bytes == 0
+    # If num_bytes_to_process is not zero, we have an incomplete character
+    return num_bytes_to_process == 0
