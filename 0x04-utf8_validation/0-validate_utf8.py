62e0c0767dd2a3ce0dc7f5d6259c5e3314166d8d
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-08-23 01:15:58 +0000

    fix

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,39 +7,33 @@ represents a valid UTF-8 encoding.
 
 def validUTF8(data):
     """
-    Validate if the given list of integers
-    represents a valid UTF-8 encoding.
+    Checks if a given list of integers represents a valid UTF-8 encoding.
 
     Args:
-        data (list of int): List where each
-        integer is a byte (0-255).
+        data (list of int): List of integers where each integer represents
+                            1 byte of data.
 
     Returns:
-        bool: True if data is valid UTF-8
-        encoding, otherwise False.
+        bool: True if data is a valid UTF-8 encoding, False otherwise.
     """
     num_bytes = 0
 
     for byte in data:
-        # Check if the byte is within the valid range
-        if byte < 0 or byte > 255:
-            return False
+        byte &= 0xFF
 
-        # Determine the number of bytes expected
         if num_bytes == 0:
-            if (byte >> 7) == 0b0:
+            if byte >> 7 == 0b0:
                 num_bytes = 0
-            elif (byte >> 5) == 0b110:
+            elif byte >> 5 == 0b110:
                 num_bytes = 1
-            elif (byte >> 4) == 0b1110:
+            elif byte >> 4 == 0b1110:
                 num_bytes = 2
-            elif (byte >> 3) == 0b11110:
+            elif byte >> 3 == 0b11110:
                 num_bytes = 3
             else:
                 return False
         else:
-            # Check if it's a valid continuation byte
-            if (byte >> 6) != 0b10:
+            if byte >> 6 != 0b10:
                 return False
             num_bytes -= 1
 
