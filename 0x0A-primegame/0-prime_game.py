71532482ca75eb25cdd474a787bd48d6006e46a8
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-10-03 19:30:20 +0000

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,51 +2,38 @@
 
 def sieve_of_eratosthenes(n):
     """Generate a list of primes up to n using the Sieve of Eratosthenes"""
-    primes = [True] * (n + 1)
+    sieve = [True] * (n + 1)
+    sieve[0] = sieve[1] = False
     p = 2
-    while (p * p <= n):
-        if (primes[p] == True):
+    while p * p <= n:
+        if sieve[p]:
             for i in range(p * p, n + 1, p):
-                primes[i] = False
+                sieve[i] = False
         p += 1
-    prime_numbers = []
-    for p in range(2, n + 1):
-        if primes[p]:
-            prime_numbers.append(p)
-    return prime_numbers
+    return sieve
 
-def count_prime_moves(n, primes):
-    """Count the number of valid moves in a game with n numbers"""
-    numbers = [True] * (n + 1)  # Represents available numbers
-    moves = 0
-    for prime in primes:
-        if prime > n:
-            break
-        if numbers[prime]:
-            # Make a move by removing prime and its multiples
-            for multiple in range(prime, n + 1, prime):
-                numbers[multiple] = False
-            moves += 1
-    return moves
+def calculate_winner(nums, prime_sieve):
+    """Determine the winner for each game"""
+    max_n = len(prime_sieve) - 1
+    # Precompute the number of prime selections up to n
+    prime_moves = [0] * (max_n + 1)
+    
+    for i in range(2, max_n + 1):
+        prime_moves[i] = prime_moves[i - 1]
+        if prime_sieve[i]:
+            # Prime number found, increase the count of moves
+            for multiple in range(i, max_n + 1, i):
+                prime_moves[multiple] += 1
 
-def isWinner(x, nums):
-    """Determine the winner after x rounds of the game"""
-    if not nums or x < 1:
-        return None
-    
-    max_n = max(nums)
-    primes = sieve_of_eratosthenes(max_n)
-    
+    # Count wins for Maria and Ben
     maria_wins = 0
     ben_wins = 0
-    
     for n in nums:
-        moves = count_prime_moves(n, primes)
-        if moves % 2 == 0:
-            ben_wins += 1
-        else:
+        if prime_moves[n] % 2 == 1:
             maria_wins += 1
-    
+        else:
+            ben_wins += 1
+
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
@@ -54,3 +41,12 @@ def isWinner(x, nums):
     else:
         return None
 
+def isWinner(x, nums):
+    """Determine the overall winner of the game"""
+    if not nums or x < 1:
+        return None
+
+    max_n = max(nums)
+    prime_sieve = sieve_of_eratosthenes(max_n)
+    
+    return calculate_winner(nums, prime_sieve)
